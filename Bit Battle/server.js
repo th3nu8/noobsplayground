94378078704e4919bdf268892e5c9acbe9d008a2
th3nu8 <blueboltgamingyt@gmail.com>
2025-10-15 21:19:42 -0500

    Update server.js

--- Bit Battle/server.js
+++ Bit Battle/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // Serve static files from the same folder
 app.use(express.static(__dirname));
