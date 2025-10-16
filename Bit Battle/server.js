79d5acfae5aa991483aed563acc5e38e202f41b3
th3nu8 <blueboltgamingyt@gmail.com>
2025-10-15 21:21:56 -0500

    Update server.js

--- Bit Battle/server.js
+++ Bit Battle/server.js
@@ -3,8 +3,8 @@ const app = express();
 const PORT = 3001;
 
 // Serve static files from the same folder
-app.use(express.static(__dirname));
-
+app.use('/', express.static(path.join(__dirname, 'main')));
+app.use('/buildnblocks', express.static(path.join(__dirname, 'public')));
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
